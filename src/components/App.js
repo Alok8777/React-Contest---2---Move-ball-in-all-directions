1bd7e9a022b4b66965829cc5b8a21a8c88d5a030
Alok8777 <68750984+Alok8777@users.noreply.github.com>
2020-11-29 00:29:01 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,81 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({
+      left: "0px",
+      top: "0px"
+    });
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 37) {
+      let value = x - 5;
+      setX(value);
+      setBallPosition({
+        left: value + "px",
+        top: setY + "px"
+      });
+    } else if (event.keyCode === 38) {
+      let value = y - 5;
+
+      setY(value);
+      setBallPosition({
+        left: setX + "px",
+        top: value + "px"
+      });
+    } else if (event.keyCode === 39) {
+      let value = x + 5;
+      setX(value);
+      setBallPosition({
+        left: value + "px",
+        top: setY + "px"
+      });
+    } else if (event.keyCode === 40) {
+      let value = y + 5;
+      setY(value);
+      setBallPosition({
+        left: setX + "px",
+        top: value + "px"
+      });
+    }
+  };
+
+  const start = () => {
+    setRenderBall(true);
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+  });
+
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else
+      return (
+        <button onClick={start} className="start">
+          Start
+        </button>
+      );
+  };
 
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
         Reset
       </button>
+
       {renderChoice()}
     </div>
   );
